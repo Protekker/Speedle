d989291d2a706f418b9ec7bf598c118c8746c189
Jakob Olson <jakob.olson@gmail.com>
2025-10-27 17:00:41 +0100

    cleaned up but not removed comments

--- Speedle.cpp
+++ Speedle.cpp
@@ -37,17 +37,18 @@ int main(){
     // string correctword = words.at(randomIndex);
     string guess;
     int lives = 6;
-    cout << "Enter a five letter word (CAPS)\n";
-    getline(cin, guess);
-    transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
-    // transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
-    auto it = find(words.begin(),words.end(),guess);
-    if (it == words.end()){
-        cout << "Word not in dictionary, retry\n";
-    }
     // cin >> guess;
+    cout << "Enter a five letter word (CAPS)\n";
+
     while(guess != correctword && lives > 0){
-        if (guess.length() == 5){
+        getline(cin, guess);
+        transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
+        // transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
+        auto it = find(words.begin(),words.end(),guess);
+        if (it == words.end()){
+            cout << "Word not in dictionary, retry\n";
+        }
+        if (guess.length() == 5 && it != words.end()){
             for(int i=0;i<5;i++)
                 if (guess[i] == correctword[i])
                     cout << "\033[32m" << guess[i] << "\033[0m"; // green
@@ -61,24 +62,24 @@ int main(){
                     break;
                 }
                 cout << "\n";
-                getline(cin, guess);
-                transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
-                // transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
-                auto it = find(words.begin(),words.end(),guess);
-                if (it == words.end()){
-                    cout << "Word not in dictionary, retry\n";
-                }
+                // getline(cin, guess);
+                // transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
+                // // transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
+                // auto it = find(words.begin(),words.end(),guess);
+                // if (it == words.end()){
+                //     cout << "Word not in dictionary, retry\n";
+                // }
                 // cin >> guess;
         }
         else{
-            cout << "Enter five letters\n";
-                getline(cin, guess);
-                transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
-                // transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
-                auto it = find(words.begin(),words.end(),guess);
-                if (it == words.end()){
-                    cout << "Word not in dictionary, retry\n";
-                }
+            cout << "Enter valid five letter word\n";
+                // getline(cin, guess);
+                // transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
+                // // transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
+                // auto it = find(words.begin(),words.end(),guess);
+                // if (it == words.end()){
+                //     cout << "Word not in dictionary, retry\n";
+                // }
             // cin >> guess; //make more readable by checking this first and the next by itself not nested if loop
         }
     }
