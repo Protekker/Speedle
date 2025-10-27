953b005fb7859c25ac45f379a4312dc5fa95375c
Jakob Olson <jakob.olson@gmail.com>
2025-10-27 15:26:15 +0100

    working version simple

--- Speedle.cpp
+++ Speedle.cpp
@@ -3,37 +3,61 @@
 #include <string.h>
 #include <vector>
 #include <random>
+#include <algorithm>
 
 using namespace std;
 
 
 int main(){
-    string correctword = "PLUMP";
-    // vector<string> words = {"APPLE","PLUMP","COULD","SHOWS","PHONE"};
+    // string correctword = "PLUMP";
+    vector<string> words = {"APPLE","PLUMP","COULD","SHOWS","PHONE"};
+    // vector<int> v = {0, 1, 2, 3, 4};
+    random_device random_device;
+    mt19937 engine{random_device()};
+    uniform_int_distribution<int> dist(0, words.size() - 1);
+    int random_element = dist(engine);
+    string correctword = words.at(random_element);
+    // string correctword = words.at(randomIndex);
     string guess;
     int lives = 6;
     cout << "Enter a five letter word (CAPS)\n";
-    cin >> guess;
+    getline(cin, guess);
+    transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
+    // cin >> guess;
     while(guess != correctword && lives > 0){
         if (guess.length() == 5){
             for(int i=0;i<5;i++)
                 if (guess[i] == correctword[i])
-                    cout << "\033[32m" << guess[i] << "\033[0m";
-                else if (correctword.find(guess[i]))
-                    cout << "\033[33m" << guess[i] << "\033[0m";
+                    cout << "\033[32m" << guess[i] << "\033[0m"; // green
+                else if (correctword.find(guess[i]) != string::npos)
+                    cout << "\033[33m" << guess[i] << "\033[0m"; // yellow
                 else
-                    cout << guess[i];
+                    cout << "\033[37m" << guess[i] << "\033[0m"; // white
                 lives--;
                 if (lives == 0){
                     cout << "\nYOU LOST";
+                    break;
                 }
                 cout << "\n";
-                cin >> guess;
+                getline(std::cin, guess);
+                transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
+
+                // cin >> guess;
         }
         else{
             cout << "Not long enough, enter five letters\n";
-            cin >> guess;
+            getline(cin, guess);
+            transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
+
+            // cin >> guess; //make more readable by checking this first and the next by itself not nested if loop
         }
+    }
+    if (guess == correctword){
+        cout << "\033[32m" << correctword << "\033[0m";
+        cout << "\nWinner!";
+    }
+    else{
+        cout << "\nBetter luck next time";
     }
 }
 
